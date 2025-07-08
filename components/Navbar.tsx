68bae9575536743589b8911f39db68117ca961b7
Shivam0710 <39822449+Shivam0710@users.noreply.github.com>
2025-07-08 11:15:40 +0530

    Updated CV

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -50,7 +50,7 @@ const Navbar = () => {
         > 
           <svg onClick={() => setIsOpen(false)} className="h-6 w-6 absolute top-3 right-4" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="50px" height="50px"><path d="M 38.982422 6.9707031 A 2.0002 2.0002 0 0 0 37.585938 7.5859375 L 24 21.171875 L 10.414062 7.5859375 A 2.0002 2.0002 0 0 0 8.9785156 6.9804688 A 2.0002 2.0002 0 0 0 7.5859375 10.414062 L 21.171875 24 L 7.5859375 37.585938 A 2.0002 2.0002 0 1 0 10.414062 40.414062 L 24 26.828125 L 37.585938 40.414062 A 2.0002 2.0002 0 1 0 40.414062 37.585938 L 26.828125 24 L 40.414062 10.414062 A 2.0002 2.0002 0 0 0 38.982422 6.9707031 z"/></svg>
           <Link
-            href="https://shivam-resume-0710.tiiny.site"
+            href="https://drive.google.com/file/d/1RagYuoisY_lDYAvpLD2CSBFZBMUlQvGO/view?usp=sharing"
             target="_blank"
             className="text-[#787878] font-semibold"
           >
@@ -86,7 +86,7 @@ const Navbar = () => {
       </div>
       <div className="hidden items-center gap-9 md:flex">
         <Link
-          href="https://shivam-resume-0710.tiiny.site"
+          href="https://drive.google.com/file/d/1RagYuoisY_lDYAvpLD2CSBFZBMUlQvGO/view?usp=sharing"
           target="_blank"
           className="text-[#787878]"
         >
