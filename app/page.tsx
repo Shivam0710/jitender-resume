b5a86df008223b758f78250628aa1a67f28e824b
Shivam0710 <39822449+Shivam0710@users.noreply.github.com>
2025-07-08 12:42:58 +0530

    Updated favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Experiences from "@/components/Experiences";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="overflow-hidden">
+      <Head>
+        <title>Jitender&apos;s Portfolio</title>
+      </Head>
       <Navbar />
       <Hero />
       <Experiences />
